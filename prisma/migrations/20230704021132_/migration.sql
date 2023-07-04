77ca53769acd1fe50fbc61aa8c5f452706f74294
Edward Lim Kok Siong <edwardlimkoksiong1994@gmail.com>
2023-07-04 10:11:55 +0800

    Initial migrate

---
+++ prisma/migrations/20230704021132_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "customer_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
